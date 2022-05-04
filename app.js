6b676d831f158e98f36dfad147f1bb563b43fee3
SanuKumar <sanukumar1138@gmail.com>
2022-05-04 07:08:55 +0530

    limiting middleware to post requests

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+//already included in express but removed in someof the versions of express, so to be sure
+const bodyParser = require("body-parser");
 //https://medium.com/the-node-js-collection/making-your-node-js-work-everywhere-with-environment-variables-2da8cdf6e786
 const dotenv = require("dotenv");
 dotenv.config();
@@ -6,14 +8,22 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/add-products", (req, res, next) => {
+app.use("/add-product", (req, res, next) => {
   console.log("In Middleware");
-  res.send("<h2>The 'Add Product' page</h2>");
+  res.send(
+    '<h2>The Add Product page</h2><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+//only get executed on post requests
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In Middleware");
   res.send("<h2>Hello from Express</h2>");
   next();
 });
